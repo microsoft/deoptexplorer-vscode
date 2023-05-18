e340617f7fd5a7439629bd1d1b6c1dadc2123c1a
Ron Buckton <ron.buckton@microsoft.com>
2023-05-18 15:20:58 -0400

    Add support for beta/dev/canary channels for Chrome and Edge

--- tools/dexnode/src/hosts/chrome.ts
+++ tools/dexnode/src/hosts/chrome.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import path from "path";
 import { type Host } from "../host.js";
 import { HostFlags } from "../hostFlags.js";
 
@@ -29,4 +30,71 @@ export const HOST_CHROME_STABLE: Host<"chrome"> = {
         { key: "SOFTWARE\\Clients\\StartMenuInternet\\Google Chrome\\shell\\open\\command" },
         { key: "SOFTWARE\\Wow6432Node\\Clients\\StartMenuInternet\\Google Chrome\\shell\\open\\command" },
     ]
+};
+
+export const HOST_CHROME_DEV: Host<"chrome_dev"> = {
+    flagName: "chrome_dev",
+    flags:
+        HostFlags.ChromiumArguments |
+        HostFlags.UseLatestV8Version,
+    paths: {
+        win32: [
+            "C:/Program Files/Google/Chrome Dev/Application/chrome.exe",
+            "C:/Program Files (x86)/Google/Chrome Dev/Application/chrome.exe",
+        ],
+        darwin: [
+            "/Applications/Google Chrome Dev.app/Contents/MacOS/Google Chrome Dev",
+        ],
+        linux: [
+            "/usr/bin/google-chrome-dev",
+            "google-chrome-dev"
+        ],
+    },
+    registry: [
+        { key: "SOFTWARE\\Clients\\StartMenuInternet\\Google Chrome Dev\\shell\\open\\command" },
+        { key: "SOFTWARE\\Wow6432Node\\Clients\\StartMenuInternet\\Google Chrome Dev\\shell\\open\\command" },
+    ]
+};
+
+export const HOST_CHROME_BETA: Host<"chrome_beta"> = {
+    flagName: "chrome_beta",
+    flags:
+        HostFlags.ChromiumArguments |
+        HostFlags.UseLatestV8Version,
+    paths: {
+        win32: [
+            "C:/Program Files/Google/Chrome Beta/Application/chrome.exe",
+            "C:/Program Files (x86)/Google/Chrome Beta/Application/chrome.exe",
+        ],
+        darwin: [
+            "/Applications/Google Chrome Beta.app/Contents/MacOS/Google Chrome Beta",
+        ],
+        linux: [
+            "/usr/bin/google-chrome-beta",
+            "google-chrome-beta"
+        ],
+    },
+    registry: [
+        { key: "SOFTWARE\\Clients\\StartMenuInternet\\Google Chrome Beta\\shell\\open\\command" },
+        { key: "SOFTWARE\\Wow6432Node\\Clients\\StartMenuInternet\\Google Chrome Beta\\shell\\open\\command" },
+    ]
+};
+
+export const HOST_CHROME_CANARY: Host<"chrome_canary"> = {
+    flagName: "chrome_canary",
+    flags:
+        HostFlags.ChromiumArguments |
+        HostFlags.UseLatestV8Version,
+    paths: {
+        win32: process.platform === "win32" && process.env.LOCALAPPDATA ?
+            [path.resolve(process.env.LOCALAPPDATA, "Google\\Chrome SxS\\Application\\chrome.exe")] :
+            undefined,
+        darwin: [], // TODO
+        linux: [
+            "/usr/bin/google-chrome-canary",
+            "google-chrome-canary"
+        ],
+    },
+    registry: [
+    ],
 };
--- tools/dexnode/src/hosts/msedge.ts
+++ tools/dexnode/src/hosts/msedge.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import path from "path";
 import { type Host } from "../host.js";
 import { HostFlags } from "../hostFlags.js";
 
@@ -26,5 +27,78 @@ export const HOST_MSEDGE_STABLE: Host<"msedge"> = {
     registry: [
         { key: "SOFTWARE\\Clients\\StartMenuInternet\\Microsoft Edge\\shell\\open\\command" },
         { key: "SOFTWARE\\Wow6432Node\\Clients\\StartMenuInternet\\Microsoft Edge\\shell\\open\\command" },
+    ],
+};
+
+export const HOST_MSEDGE_DEV: Host<"msedge_dev"> = {
+    flagName: "msedge_dev",
+    flags:
+        HostFlags.ChromiumArguments |
+        HostFlags.UseLatestV8Version,
+    paths: {
+        win32: [
+            "C:/Program Files/Microsoft/Edge Dev/Application/msedge.exe",
+            "C:/Program Files (x86)/Microsoft/Edge Dev/Application/msedge.exe",
+        ],
+        darwin: [
+            "/Applications/Microsoft Edge Dev.app/Contents/MacOS/Microsoft Edge Dev",
+        ],
+        linux: [
+            "/opt/microsoft/msedge-dev/msedge-dev",
+            "/opt/microsoft/msedge-dev/msedge",
+            "/usr/bin/microsoft-edge-dev",
+            "microsoft-edge-dev"
+        ],
+    },
+    registry: [
+        { key: "SOFTWARE\\Clients\\StartMenuInternet\\Microsoft Edge Dev\\shell\\open\\command" },
+        { key: "SOFTWARE\\Wow6432Node\\Clients\\StartMenuInternet\\Microsoft Edge Dev\\shell\\open\\command" },
+    ],
+};
+
+export const HOST_MSEDGE_BETA: Host<"msedge_beta"> = {
+    flagName: "msedge_beta",
+    flags:
+        HostFlags.ChromiumArguments |
+        HostFlags.UseLatestV8Version,
+    paths: {
+        win32: [
+            "C:/Program Files/Microsoft/Edge Beta/Application/msedge.exe",
+            "C:/Program Files (x86)/Microsoft/Edge Beta/Application/msedge.exe",
+        ],
+        darwin: [
+            "/Applications/Microsoft Edge Beta.app/Contents/MacOS/Microsoft Edge Beta",
+        ],
+        linux: [
+            "/opt/microsoft/msedge-beta/msedge-beta",
+            "/opt/microsoft/msedge-beta/msedge",
+            "/usr/bin/microsoft-edge-beta",
+            "microsoft-edge-beta"
+        ],
+    },
+    registry: [
+        { key: "SOFTWARE\\Clients\\StartMenuInternet\\Microsoft Edge Beta\\shell\\open\\command" },
+        { key: "SOFTWARE\\Wow6432Node\\Clients\\StartMenuInternet\\Microsoft Edge Beta\\shell\\open\\command" },
+    ],
+};
+
+export const HOST_MSEDGE_CANARY: Host<"msedge_canary"> = {
+    flagName: "msedge_canary",
+    flags:
+        HostFlags.ChromiumArguments |
+        HostFlags.UseLatestV8Version,
+    paths: {
+        win32: process.platform === "win32" && process.env.LOCALAPPDATA ?
+            [path.resolve(process.env.LOCALAPPDATA, "Microsoft\\Edge SxS\\Application\\msedge.exe")] :
+            undefined,
+        darwin: [], // TODO
+        linux: [
+            "/opt/microsoft/msedge-canary/msedge-canary",
+            "/opt/microsoft/msedge-canary/msedge",
+            "/usr/bin/microsoft-edge-canary",
+            "microsoft-edge-canary"
+        ],
+    },
+    registry: [
     ],
 };
